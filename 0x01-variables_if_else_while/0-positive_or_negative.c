bd3a1431438c4039a6f02790146c8a3eda4ebd14
techeo <tegaecheo@gmail.com>
2023-10-08 06:40:34 -0700

    0-positive_or_negative.c edited

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,8 @@ int main(void)
 {
 	int n;
 
-	/* Seed the random number generator with the current time */
 	srand(time(0));
 
-	/* Generate a random number and shift it to be within -RAND_MAX/2 to +RAND_MAX/2 range */
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
