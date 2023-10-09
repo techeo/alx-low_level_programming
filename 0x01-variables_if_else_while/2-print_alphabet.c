93bb47e913b1a5ca748bff25870c899da8f4e156
techeo <tegaecheo@gmail.com>
2023-10-09 15:57:42 -0700

    alphabets task

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * main - Entry point of program
  *
  *
  * Return: Always 0 (Sucess)
  */
-int main(void) {
-    char letter = 'a';
 
-    while (letter <= 'z') {
-        putchar(letter);
-        letter++;
+int main(void)
+{
+    // Declare a character variable 'c' to store the current letter
+    char c;
+
+    // Start a loop to iterate from 'a' to 'z'
+    for (c = 'a'; c <= 'z'; c++)
+    {
+        // Print the current letter using the putchar function
+        putchar(c);
     }
 
+    // Print a newline character to move to the next line
     putchar('\n');
 
-    return 0;
+    // Return 0 to indicate successful program execution
+    return (0);
 }
+
