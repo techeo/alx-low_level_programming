01e64d597cda2c4c887664e63438108672e11892
techeo <tegaecheo@gmail.com>
2023-10-10 17:55:33 -0700

    2-alphabet task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print the lowercase alphabet ten times.
+ */
+void print_alphabet_x10(void)
+{
+    char letter;
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        letter = 'a';
+        while (letter <= 'z')
+        {
+            _putchar(letter);
+            letter++;
+        }
+        _putchar('\n');
+    }
+}
