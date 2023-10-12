0ee1ec91eecaa1a57832f0cdd2e44a949a8bbc65
techeo <tegaecheo@gmail.com>
2023-10-12 15:33:26 -0700

    6-print_line.c task

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_line - Draw a straight line of underscores
+ */
+void print_line(int n)
+{
+	while (!(n <= 0))
+	{
+		_putchar('_');
+		n--;
+}
+	_putchar('\n');
+}
