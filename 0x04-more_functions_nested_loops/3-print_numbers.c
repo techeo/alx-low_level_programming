e49608429b3e20ff7faf876877acae0088a094cb
techeo <tegaecheo@gmail.com>
2023-10-12 15:05:55 -0700

    3-print_number.c task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print the numbers from 0 to 9 followed by a new line.
+ */
+void print_numbers(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		_putchar(num);
+		num++;
+	}
+	_putchar('\n');
+}
