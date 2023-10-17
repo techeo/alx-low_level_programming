42cf388db21a5ccb454674d5f76352c520d86cfb
techeo <tegaecheo@gmail.com>
2023-10-16 20:44:46 -0700

    0 task observations

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * reset_to_98 - a function that sets a pointer to 98.
+  * reset_to_98 - a function that sets a pointer.
   * @n: pointer to change
   */
 void reset_to_98(int *n)
