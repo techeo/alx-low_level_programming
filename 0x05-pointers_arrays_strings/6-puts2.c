4f1c1f530abd7fc47822e6feed67f25da4fe2d97
techeo <tegaecheo@gmail.com>
2023-10-16 19:48:01 -0700

    correction

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main."
+
+/**
+ * puts2 - every other character.
+ * @str: string to print.
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0' ; i++)
+	{
+		if ((i % 2) == 0)
+			_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
