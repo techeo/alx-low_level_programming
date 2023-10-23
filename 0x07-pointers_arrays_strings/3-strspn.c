2f940fc94c649a7fac6de94b46846e82622ec1c0
techeo <tegaecheo@gmail.com>
2023-10-23 16:01:16 -0700

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: the string to search
+ * @accept: the string containing characters to be matched
+ *
+ * Return: number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	int match;
+
+	while (*s)
+	{
+		match = 0;
+		while (*accept)
+		{
+			if (*s == *accept)
+			{
+				count++;
+				match = 1;
+				break;
+			}
+			accept++;
+		}
+		if (match == 0)
+			return count;
+		s++;
+	}
+
+	return count;
+}
