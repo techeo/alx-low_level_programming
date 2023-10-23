db0345aa151dfe3955ccde7a7fbe128d09c2ebd1
techeo <tegaecheo@gmail.com>
2023-10-23 15:56:30 -0700

    task 2 corrections

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,12 @@
-#include "nain.h"
+#include "main.h"
+#include <stddef.h>
 
 /**
  * _strchr - locates a character in a string
  * @s: the string to search
  * @c: the character to find
  *
- * Return: a pointer to the first occurrence of the character c in s, or NULL if not found
+ * Return: a pointer
  */
 char *_strchr(char *s, char c)
 {
