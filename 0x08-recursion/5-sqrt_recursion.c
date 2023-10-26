cac85cc06ff37f5fca8784e7d2b438f483ab39df
techeo <tegaecheo@gmail.com>
2023-10-25 17:20:32 -0700

    task 5 modifications

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 int sqrt_rec_odd(int start, int n);
-
 /**
- * _sqrt_recursion - find the square root of a given number.
- * @n: number to find the square root of.
+ * _sqrt_recursion - find the square root of a number.
+ * @n: the number
  *
  * Return: the square root as an integer.
  */
 int _sqrt_recursion(int n)
 {
+	int sqrt;
+
 	if (n < 0)
 		return (-1);
-	else if (n <= 1)
+	else if (n <= 1 && n >= 0)
 		return (n);
 
-	if (n % 2 == 0)
+	if (!(n % 2))
 	{
 		if (n / 2 == 2 || n == 2)
 			return (2);
-		int sqrt = _sqrt_recursion(n / 2);
-
+		sqrt = _sqrt_recursion(n / 2);
 		if (n / sqrt == sqrt)
 			return (sqrt);
 		else if (sqrt > 1)
@@ -34,9 +34,9 @@ int _sqrt_recursion(int n)
 }
 
 /**
- * sqrt_rec_odd - helper to deal with odd numbers and primes.
+ * sqrt_rec_odd - helper function
  * @start: the odd or prime number to check for sqrt.
- * @n: number to find the square root of.
+ * @n: the number
  *
  * Return: the square root as an integer.
  */
