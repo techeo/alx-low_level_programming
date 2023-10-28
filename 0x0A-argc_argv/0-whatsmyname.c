d290c90a961e009bad72a4ca61400f805fcd1769
techeo <tegaecheo@gmail.com>
2023-10-27 19:28:48 -0700

    0-whatsmyname.c task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
