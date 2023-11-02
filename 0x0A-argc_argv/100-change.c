3f8c069365cd6169128cc20752605c36bb6ccbb8
techeo <tegaecheo@gmail.com>
2023-11-01 19:30:42 -0700

    task 1000 updated

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,41 +2,75 @@
 #include <stdlib.h>
 #include "main.h"
 
+int get_minimum_coins(int money);
+
 /**
- * main - Entry point.
- * @argc: The number of command-line arguments.
- * @argv: An array containing the command-line arguments.
+ * main - Calculate the minimum number of coins to make change.
+ * @argc: Number of arguments.
+ * @argv: Amount of money in singles.
  *
- * Return: 0 for success, 1 for error.
+ * Return: 0 (success), 1 (failure)
  */
 int main(int argc, char *argv[])
 {
+	int money, change;
+
+	money = change = 0;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int cents = atoi(argv[1]);
+	money = atoi(argv[1]);
+	change = get_minimum_coins(money);
 
-	if (cents < 0)
+	printf("%d\n", change);
+	return (0);
+}
+
+/**
+ * get_minimum_coins - Calculate the minimum number of coins to make change.
+ * @money: Amount of money in singles.
+ *
+ * Return: Minimum number of coins (int)
+ */
+int get_minimum_coins(int money)
+{
+	int one, two, five, ten, twenty_five, sum;
+
+	sum = one = two = five = ten = twenty_five = 0;
+
+	if (money <= 0)
 	{
-		printf("0\n");
 		return (0);
 	}
 
-	int coins[] = {25, 10, 5, 2, 1};
-	int min_coins = 0;
-	int i = 0;
-
-	while (cents > 0)
+	if (money >= 25)
 	{
-		min_coins += cents / coins[i];
-		cents %= coins[i];
-		i++;
+		twenty_five = money / 25;
+		money -= twenty_five * 25;
 	}
 
-	printf("%d\n", min_coins);
+	if (money < 25 && money >= 10)
+	{
+		ten = money / 10;
+		money -= ten * 10;
+	}
 
-	return (0);
+	if (money < 10 && money >= 5)
+	{
+		five = money / 5;
+		money -= five * 5;
+	}
+	if (money < 5 && money >= 2)
+		two = money / 2;
+	money -= two * 2;
+	if (money < 2)
+		one = money;
+
+	sum = twenty_five + ten + five + two + one;
+	return (sum);
 }
+
