53641910708180984f87017695be57e80d47f580
techeo <tegaecheo@gmail.com>
2023-11-16 15:26:39 -0800

    fixes

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,18 @@
 #include <stdlib.h>
 
-typedef struct list {
-	char *str;
-	int len;
-	struct list *next;
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
 int _putchar(char c);
