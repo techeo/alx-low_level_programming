07eeb89f9d9bc079e3fc8c8ccc332d0e26eec9f0
techeo <tegaecheo@gmail.com>
2023-11-16 17:15:12 -0800

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
+int main()
+{
+	printf("Inside main()\n");
+	return (0);
+}
