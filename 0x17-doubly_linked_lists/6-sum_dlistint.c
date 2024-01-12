3df90c00a90234e2359a1689895325647eb6c102
techeo <tegaecheo@gmail.com>
2024-01-12 12:55:40 -0800

    6-sum_dlistint.c task

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of all the data in the list.
+ * @head: pointer to the head of the list.
+ * Return: sum of all data in the list.
+ **/
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *currentNode = head;
+	int sum = 0;
+
+	while (currentNode)
+	{
+		sum += currentNode->n;
+		currentNode = node->next;
+	}
+
+	return (sum);
+}
