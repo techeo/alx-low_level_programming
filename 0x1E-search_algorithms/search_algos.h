375536f86a05aa9919d76b86395b4ab3cb89e1c3
techeo <tegaecheo@gmail.com>
2024-05-09 05:21:45 -0700

    update header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -30,7 +30,7 @@ typedef struct listint_s
  * @express: Pointer to the next node in the express lane
  *
  * Description: singly linked list node structure with an express lane
- * for Holberton project
+ * for alx project
  */
 typedef struct skiplist_s
 {
@@ -48,3 +48,5 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
+
+#endif
